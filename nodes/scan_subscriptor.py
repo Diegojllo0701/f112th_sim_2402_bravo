91e0f5fd317cff07fc8505d277584f343836b869
diego <diegojaramillo0701@gmail.com>
2024-07-31 20:56:42 -0400

    scan_subscriptor

--- nodes/scan_subscriptor.py
+++ nodes/scan_subscriptor.py
@@ -1,11 +1,13 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float32MultiArray
 
 class ScanReader(Node):
     def __init__(self):
         super().__init__('scan_reader')
-        self.subscription = self.create_subscription(LaserScan,"/scan",self.scan_callback,10)
+        self.subscription = self.create_subscription(LaserScan, "/scan", self.scan_callback, 10)
+        self.publisher = self.create_publisher(Float32MultiArray, '/angles', 10)
         self.subscription  # prevent unused variable warning
 
     def scan_callback(self, msg: LaserScan):
@@ -13,6 +15,9 @@ class ScanReader(Node):
         distances = self.get_distances_at_angles(msg, angles_to_check)
         for angle, distance in distances.items():
             self.get_logger().info(f'Distance at {angle} degrees: {distance:.2f} meters')
+        
+        angles_array = Float32MultiArray(data=angles_to_check)
+        self.publisher.publish(angles_array)
 
     def get_distances_at_angles(self, msg, angles):
         distances = {}
